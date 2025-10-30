cebce4c7875b84bd67ffa4d8bc36d261cb090811
stevetanex <113615775+stevetanex@users.noreply.github.com>
2025-10-30 19:00:34 +0530

    V8

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { AuthProvider } from './context/AuthContext.jsx' // <-- IMPORT
+import { AuthProvider } from './contest/authcontext.jsx' // <-- IMPORT
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
